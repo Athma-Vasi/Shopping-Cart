ddbfb5ab68dfde777dec184d2940193d5062470c
Athma Chidambaravasi <athma.vasi@protonmail.com>
2022-07-02 15:51:02 -0600

    add api data and types in skeleton file

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import './App.css'
-import { accessoriesData } from './helpers/data/accessoriesData'
 
 function App() {
+	const pexels = (async function () {
+		const response = await fetch(
+			'https://api.pexels.com/v1/search?query=shoes&size=small&orientation=square&per_page=16&page=1',
+			{
+				headers: {
+					Authorization: '563492ad6f9170000100000133155030ab404675883db9f894b00013',
+				},
+			}
+		)
+		const data = await response.json()
+		console.log(data)
+	})()
+
 	return (
 		<div className="App">
-			<h5>28405745-afba05afaff6b2d73c3e52d3d</h5>
-			{Object.entries(Object.fromEntries(accessoriesData)).map(([key, value]) => {
-				return (
-					<div key={key}>
-						{Object.entries(Object.fromEntries(value)).map(([key_, value_]) => {
-							return key_ === 'src' ? (
-								<img key={key_} src={value_} alt="" />
-							) : (
-								<p key={key_}>{value_}</p>
-							)
-						})}
-					</div>
-				)
-			})}
+			<h5>563492ad6f9170000100000133155030ab404675883db9f894b00013</h5>
 		</div>
 	)
 }
--- src/helpers/types.d.ts
+++ src/helpers/types.d.ts
@@ -1,3 +1,34 @@
-type ItemData = Map<number, Map<string, string>>
+interface PhotosResponse {
+	page: number
+	per_page: number
+	photos: Photo[]
+	total_results: number
+	next_page: string
+}
 
-export { ItemData }
+interface Photo {
+	id: number
+	width: number
+	height: number
+	url: string
+	photographer: string
+	photographer_url: string
+	photographer_id: number
+	avg_color: string
+	src: Src
+	liked: boolean
+	alt: string
+}
+
+interface Src {
+	original: string
+	large2x: string
+	large: string
+	medium: string
+	small: string
+	portrait: string
+	landscape: string
+	tiny: string
+}
+
+export { PhotosResponse, Photo, Src }
