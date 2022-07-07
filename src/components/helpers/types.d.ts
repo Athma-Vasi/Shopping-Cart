a4b7d06610ebdf5e41dc7dafdacaa2e52946192c
Athma Chidambaravasi <athma.vasi@protonmail.com>
2022-07-07 15:18:16 -0600

    remove sampleData file and corresponding types

--- src/components/helpers/types.d.ts
+++ src/components/helpers/types.d.ts
@@ -1,46 +1,5 @@
-interface PhotosResponse {
-	page: number
-	per_page: number
-	photos: Photo[]
-	total_results: number
-	next_page: string
-}
-
-interface Photo {
-	id: number
-	width: number
-	height: number
-	url: string
-	photographer: string
-	photographer_url: string
-	photographer_id: number
-	avg_color: string
-	src: Src
-	liked: boolean
-	alt: string
-}
-
-interface Src {
-	original: string
-	large2x: string
-	large: string
-	medium: string
-	small: string
-	portrait: string
-	landscape: string
-	tiny: string
-}
-
 type ItemData = Map<number, Map<string, string>>
 
-// type State = {
-// 	itemCategory: string
-// 	itemId: string
-// 	itemName: string
-// 	itemPrice: string
-// 	itemDescription: string
-// 	itemAmount: string
-// }[]
 type ThemeState = {
 	colour: {
 		light: string
@@ -79,4 +38,4 @@ type Action = {
 	toggleTheme: 'toggleTheme'
 }
 
-export { PhotosResponse, Photo, Src, ItemData, State, Dispatch, Action, ThemeState }
+export { ItemData, State, Dispatch, Action, ThemeState }
